5b4322163d9e8aa3b422fd39d19a8b6a0ce7fac5
Merry M <Merry.j.mitchell2@gmail.com>
2025-05-14 10:47:51 +0200

    adresse,salgsaftale,Model

--- src/main/java/com/example/bilabonnement_dk/model/Adresse.java
+++ src/main/java/com/example/bilabonnement_dk/model/Adresse.java
@@ -1,4 +1,61 @@
 package com.example.bilabonnement_dk.model;
 
 public class Adresse {
+    private int adresse_ID;
+    private int vejnr;
+    private String vejnavn;
+    private String land;
+    private int postnr;
+
+
+    public Adresse() {
+    }
+
+public Adresse(int adresse_ID, int vejnr, String vejnavn, String land, int postnr) {
+    this.adresse_ID = adresse_ID;
+    this.vejnr = vejnr;
+    this.vejnavn = vejnavn;
+    this.land = land;
+    this.postnr = postnr;
+    }
+
+    public int getAdresse_ID() {
+        return adresse_ID;
+    }
+
+    public int getVejnr() {
+        return vejnr;
+    }
+
+    public String getVejnavn() {
+        return vejnavn;
+    }
+
+    public String getLand() {
+        return land;
+    }
+
+    public int getPostnr() {
+        return postnr;
+    }
+
+    public void setAdresse_ID(int adresse_ID) {
+        this.adresse_ID = adresse_ID;
+    }
+
+    public void setVejnr(int vejnr) {
+        this.vejnr = vejnr;
+    }
+
+    public void setVejnavn(String vejnavn) {
+        this.vejnavn = vejnavn;
+    }
+
+    public void setLand(String land) {
+        this.land = land;
+    }
+
+    public void setPostnr(int postnr) {
+        this.postnr = postnr;
+    }
 }
--- src/main/java/com/example/bilabonnement_dk/model/Salgsaftale.java
+++ src/main/java/com/example/bilabonnement_dk/model/Salgsaftale.java
@@ -1,4 +1,80 @@
 package com.example.bilabonnement_dk.model;
+import java.util.Date;
 
 public class Salgsaftale {
+    private int salgs_ID;
+    private double salgspris;
+    private Date leveringsdato;
+    private Adresse adresse;
+    private Medarbejder medarbejder;
+    private Bil bil;
+    private Kunde kunde;
+
+    public Salgsaftale(){}
+
+    public Salgsaftale(int salgs_ID, double salgspris, Date leveringsdato, Adresse adresse, Medarbejder medarbejder, Bil bil, Kunde kunde){
+        this.salgs_ID = salgs_ID;
+        this.salgspris = salgspris;
+        this.leveringsdato = leveringsdato;
+        this.adresse = adresse;
+        this.medarbejder = medarbejder;
+        this.bil = bil;
+        this.kunde = kunde;
+    }
+
+    public int getSalgs_ID() {
+        return salgs_ID;
+    }
+
+    public double getSalgspris() {
+        return salgspris;
+    }
+
+    public Date getLeveringsdato() {
+        return leveringsdato;
+    }
+
+    public Adresse getAdresse() {
+        return adresse;
+    }
+
+    public Medarbejder getMedarbejder() {
+        return medarbejder;
+    }
+
+    public Bil getBil() {
+        return bil;
+    }
+
+    public Kunde getKunde() {
+        return kunde;
+    }
+
+    public void setSalgs_ID(int salgs_ID) {
+        this.salgs_ID = salgs_ID;
+    }
+
+    public void setSalgspris(double salgspris) {
+        this.salgspris = salgspris;
+    }
+
+    public void setLeveringsdato(Date leveringsdato) {
+        this.leveringsdato = leveringsdato;
+    }
+
+    public void setAdresse(Adresse adresse) {
+        this.adresse = adresse;
+    }
+
+    public void setMedarbejder(Medarbejder medarbejder) {
+        this.medarbejder = medarbejder;
+    }
+
+    public void setBil(Bil bil) {
+        this.bil = bil;
+    }
+
+    public void setKunde(Kunde kunde) {
+        this.kunde = kunde;
+    }
 }
