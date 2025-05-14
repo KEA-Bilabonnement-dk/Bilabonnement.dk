10fc94b3696a670049c21c7fbbc23fc0073fdfe2
kmeye <kmeyersteffensen@gmail.com>
2025-05-14 11:46:07 +0200

    Ændringer til model-laget

--- src/main/java/com/example/bilabonnement_dk/model/Abonnementstype.java
+++ src/main/java/com/example/bilabonnement_dk/model/Abonnementstype.java
@@ -1,36 +1,40 @@
 package com.example.bilabonnement_dk.model;
 
 public class Abonnementstype {
-    private String abonnementstype_ID;
-    private Enum type;
+    private int abonnementstype_ID;
+    private Type type;
     private boolean kanForhaandSealges;
 
-    public Abonnementstype() {}
+    public Abonnementstype() {
+    }
 
-    public Abonnementstype(int abonnementstype_ID, Enum type, boolean kanForhaandSealges) {
-        this.abonnementstype_ID = String.valueOf(abonnementstype_ID);
+    public Abonnementstype(int abonnementstype_ID, Type type, boolean kanForhaandSealges) {
+        this.abonnementstype_ID = abonnementstype_ID;
         this.type = type;
         this.kanForhaandSealges = kanForhaandSealges;
     }
-    public String getAbonnementstype_ID() {
-        return abonnementstype_ID;
 
+    public int getAbonnementstype_ID() {
+        return abonnementstype_ID;
     }
-    public void setAbonnementstype_ID(String abonnementstype_ID) {
+
+    public void setAbonnementstype_ID(int abonnementstype_ID) {
         this.abonnementstype_ID = abonnementstype_ID;
     }
-    public Enum getType() {
+
+    public Type getType() {
         return type;
     }
-    public void setType(Enum type) {
+
+    public void setType(Type type) {
         this.type = type;
     }
+
     public boolean isKanForhaandSealges() {
         return kanForhaandSealges;
     }
+
     public void setKanForhaandSealges(boolean kanForhaandSealges) {
         this.kanForhaandSealges = kanForhaandSealges;
     }
-
 }
-
--- src/main/java/com/example/bilabonnement_dk/model/Reservedel.java
+++ src/main/java/com/example/bilabonnement_dk/model/Reservedel.java
@@ -1,25 +1,25 @@
 package com.example.bilabonnement_dk.model;
 
 public class Reservedel {
-    private int reservedel_id;
+    private int reservedel_ID;
     private String type;
     private double pris;
 
     public Reservedel() {}
 
-    public Reservedel(int reservedel_id, String type, double pris) {
-        this.reservedel_id = reservedel_id;
+    public Reservedel(int reservedel_ID, String type, double pris) {
+        this.reservedel_ID = reservedel_ID;
         this.type = type;
         this.pris = pris;
 
     }
 
-    public int getReservedel_id() {
-        return reservedel_id;
+    public int getReservedel_ID() {
+        return reservedel_ID;
     }
 
-    public void setReservedel_id(int reservedel_id) {
-        this.reservedel_id = reservedel_id;
+    public void setReservedel_id(int reservedel_ID) {
+        this.reservedel_ID = reservedel_ID;
     }
 
     public String getType() {
